b2fa3a6b4f45bbc36818d39d725d788ebcc522d3
Vince Adamo <vince@hellotherma.com>
2023-11-14 22:30:09 -0500

    explicity check for user not found

--- auth-proxy/src/main/java/com/vinceadamo/authproxy/authproxy/services/UserService.java
+++ auth-proxy/src/main/java/com/vinceadamo/authproxy/authproxy/services/UserService.java
@@ -8,6 +8,7 @@ import java.net.http.HttpResponse.BodyHandlers;
 import java.util.UUID;
 
 import org.springframework.cloud.gateway.support.NotFoundException;
+import org.springframework.http.HttpStatus;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vinceadamo.authproxy.authproxy.jsonobjects.User;
@@ -19,7 +20,7 @@ import org.slf4j.LoggerFactory;
 public class UserService {
     static Logger logger = LoggerFactory.getLogger(UserService.class);
 
-    public static User read(UUID id) throws Exception {
+    public static User read(UUID id) throws NotFoundException, Exception {
         try {
             ObjectMapper mapper = new ObjectMapper();
 
@@ -34,12 +35,23 @@ public class UserService {
 
             HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 
-            logger.debug(response.body());
+            int statusCode = response.statusCode();
 
-            return mapper.readValue(response.body(), User.class);
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                logger.warn("User " + id + " not found!");
+                throw new NotFoundException("User " + id + " not found!");
+            }
+
+            String responseBody = response.body();
+
+            logger.debug(responseBody);
+
+            return mapper.readValue(responseBody, User.class);
+        } catch (NotFoundException e) {
+            throw e;
         } catch (Exception e) {
             logger.error(e.getMessage());
-            throw new NotFoundException("User " + id + " not found!");
+            throw e;
         }
     }
 }
